45a08e52fa503d1cba1667b90f7586280af43e21
Egor <egor.krutoychel.komp@mail.ru>
2018-02-18 01:53:05 +0300

    added some commands

--- src/main/java/by/makedon/smtpclient/command/impl/SendCommand.java
+++ src/main/java/by/makedon/smtpclient/command/impl/SendCommand.java
@@ -1,13 +1,33 @@
 package by.makedon.smtpclient.command.impl;
 
 import by.makedon.smtpclient.command.Command;
+import by.makedon.smtpclient.exception.CommandException;
+import by.makedon.smtpclient.exception.InvalidParameterException;
 import by.makedon.smtpclient.model.ParameterCriteria;
+import by.makedon.smtpclient.model.Validator;
+import by.makedon.smtpclient.socket.MailSocket;
 
 import java.util.Map;
 
 public class SendCommand implements Command {
-    @Override
-    public void execute(Map<ParameterCriteria, String> parameters) {
+    private static final String SEND = "SEND FROM:<%s>\r\n";
 
+    @Override
+    public void execute(Map<ParameterCriteria, String> parameters) throws InvalidParameterException, CommandException {
+        String argumentValue = parameters.get(ParameterCriteria.ARGUMENT);
+
+        if (!Validator.validateEmail(argumentValue)) {
+                throw new InvalidParameterException("invalid from email");
+        }
+
+        String send = String.format(SEND, argumentValue);
+
+        MailSocket mailSocket = MailSocket.getInstance();
+        try {
+            executeCommand(send);
+        } catch (Exception e) {
+            mailSocket.close();
+            throw new CommandException(e);
+        }
     }
 }
--- src/main/java/by/makedon/smtpclient/command/impl/SomlCommand.java
+++ src/main/java/by/makedon/smtpclient/command/impl/SomlCommand.java
@@ -1,14 +1,33 @@
 package by.makedon.smtpclient.command.impl;
 
 import by.makedon.smtpclient.command.Command;
+import by.makedon.smtpclient.exception.CommandException;
+import by.makedon.smtpclient.exception.InvalidParameterException;
 import by.makedon.smtpclient.model.ParameterCriteria;
+import by.makedon.smtpclient.model.Validator;
+import by.makedon.smtpclient.socket.MailSocket;
 
 import java.util.Map;
 
 public class SomlCommand implements Command {
+    private static final String SOML = "SOML FROM:<%s>\r\n";
 
     @Override
-    public void execute(Map<ParameterCriteria, String> parameters) {
+    public void execute(Map<ParameterCriteria, String> parameters) throws InvalidParameterException, CommandException {
+        String argumentValue = parameters.get(ParameterCriteria.ARGUMENT);
 
+        if (!Validator.validateEmail(argumentValue)) {
+            throw new InvalidParameterException("invalid from email");
+        }
+
+        String soml = String.format(SOML, argumentValue);
+
+        MailSocket mailSocket = MailSocket.getInstance();
+        try {
+            executeCommand(soml);
+        } catch (Exception e) {
+            mailSocket.close();
+            throw new CommandException(e);
+        }
     }
 }
